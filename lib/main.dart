929f372130d9eba3ffddfb2d1db6bdb2aaa0208b
rajuislamdev <rajuislam.dev@gmail.com>
2024-07-16 15:47:05 +0600

    fix the project running issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'src/_route/routes.dart';
 import 'src/bindings/init_bindings.dart';
@@ -12,10 +14,12 @@ import 'src/languages/language_translation.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initialConfig();
-  // await Firebase.initializeApp();
-  // await GetStorage.init();
+  await Firebase.initializeApp();
+  await GetStorage.init();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((_) {});
+      .then((_) {
+    runApp(MyApp());
+  });
 }
 
 Future<void> initialConfig() async {
